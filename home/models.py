173aac65366cc6f0ea7e8a4245d3cc8317dfd712
Shishir_Kc <shishir.khatri.kc@gmail.com>
2025-07-28 20:25:24 +0545

    added some db by deafut is Sql lite

--- home/models.py
+++ home/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class User_Account(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    profile_pic = models.ImageField(upload_to='user/')
+
+
+    def __str__(self):
+        return self.user.username
+    
+    
