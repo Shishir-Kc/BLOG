21933b58c001dea2cac8611e2a26fdb21e9e398a
Shishir_Kc <shishir.khatri.kc@gmail.com>
2025-07-30 18:55:40 +0545

    made some changes in models

--- home/models.py
+++ home/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
 
 class User_Account(models.Model):
     user = models.ForeignKey(User,on_delete=models.CASCADE)
@@ -10,3 +11,17 @@ class User_Account(models.Model):
         return self.user.username
     
     
+class User_content(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    post_title = models.CharField(max_length=50,verbose_name='title')
+    content = models.TextField(verbose_name='content')
+    content_image = models.ImageField(upload_to='user/content/images',blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = 'user_post'
+        verbose_name_plural = 'posts'
+
+    def __str__(self):
+        return f' {self.user.username} | '
+ 
