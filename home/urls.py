572524d5b6788632af1849c705b53d17b3ef66ec
Shishir_Kc <shishir.khatri.kc@gmail.com>
2025-07-30 18:54:37 +0545

    added some routings

--- home/urls.py
+++ home/urls.py
@@ -5,5 +5,11 @@ app_name = 'home'
 
 urlpatterns = [
         path('',views.home,name="home"),
-        path('api/latest/',views.raw_data),
+        path('user/upload',views.upload_content,name='content_upload'),
+        path('api/user/',views.raw_data),
+        path('api/login/',views.login_user),
+        path('api/logout/',views.Logout_user),
+        path('api/upload/content/',views.User_content_upload),
+        path('api/content/',views.User_feed),
+
 ]
